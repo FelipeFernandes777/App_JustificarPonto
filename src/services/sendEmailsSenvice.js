d8b3c630a521aeee75dbe5e68d0c3720a247584f
FelipeFernandes777 <felipe524.fenrandes@gmail.com>
2025-02-03 09:18:57 -0300

    fix: html in email

--- src/services/sendEmailsSenvice.js
+++ src/services/sendEmailsSenvice.js
@@ -18,34 +18,111 @@ export async function sendEmailsSevices(t) {
       console.log(timeInfo.data);
 
       const formatedText = `
-         ${getGreeting()}
+<!DOCTYPE html>
+<html>
+   <head>
+      <meta charset="UTF-8" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <title>Registro de Ponto</title>
+      <style>
+         body {
+            font-family: Arial, sans-serif;
+            margin: 20px;
+            padding: 20px;
+            background-color: #f4f4f4;
+         }
+         .container {
+            background: #fff;
+            padding: 20px;
+            border-radius: 8px;
+            box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+            max-width: 600px;
+            margin: auto;
+         }
+         .title {
+            font-size: 18px;
+            font-weight: bold;
+            margin-bottom: 10px;
+         }
+         .info {
+            margin-bottom: 8px;
+         }
 
-         Data: ${formatedDate(timeInfo.data)}
-         Nome do Funcionário: ${userInfo.nome_funcionario}
-         Empresa: ${userInfo.empresa}
+         .info_content {
+            width: 100%;
+            height: 100%;
+         }
+         .info_checkbox {
+            width: 100%;
+            font-weight: bold;
+            display: flex;
+            gap: 10px;
+            justify-content: space-between;
+         }
 
-         ${timeInfo.entrada ? "( X )" : "( )"} Entrada: Horário: ${
-         timeInfo.entradaTime
-      }
-         ${timeInfo.intervalo ? "( X )" : "( )"} Intervalo: Horário: ${
-         timeInfo.intervaloTime
-      }
-         ${timeInfo.retorno ? "( X )" : "( )"} Retorno: Horário: ${
-         timeInfo.retornoTime
-      }
-         ${timeInfo.saida ? "( X )" : "( )"} Saída: Horário: ${
-         timeInfo.saidaTime
-      }
+         .info_option{
+            width: 90px;
+         }
+         .space {
+            width: 120px;
+         }
+      </style>
+   </head>
+   <body>
+      <div class="container">
+         <div class="title">${getGreeting()}</div>
+         <div class="info">Data: ${formatedDate(timeInfo.data)}</div>
+         <div class="info">
+            Nome do Funcionário: ${userInfo.nome_funcionario}
+         </div>
+         <div class="info">Empresa: ${userInfo.empresa}</div>
 
-         Justificativa: ${userInfo.justificativa}
+         <div class="info_content">
+            <div class="info_checkbox">
+               <span class="info_option">${
+                  timeInfo.entrada ? "( X )" : "( )"
+               } Entrada: </span>
+               <div class="space"> </div>
+               <span>Horário: ${timeInfo.entradaTime} </span>
+            </div>
+            <div class="info_checkbox">
+               <span class="info_option">${
+                  timeInfo.intervalo ? "( X )" : "( )"
+               } Intervalo: </span>
+               <div class="space"> </div>
+               <span>Horário: ${timeInfo.intervaloTime}</span>
+            </div>
+            <div class="info_checkbox">
+               <span class="info_option">${
+                  timeInfo.retorno ? "( X )" : "( )"
+               } Retorno: </span>
+               <div class="space"> </div>
+               <span>Horário: ${timeInfo.retornoTime}</span>
+            </div>
+            <div class="info_checkbox">
+               <span class="info_option">${
+                  timeInfo.saida ? "( X )" : "( )"
+               } Saída:</span>
+               <div class="space"> </div>
+               <span> Horário: ${timeInfo.saidaTime}</span>
+            </div>
+         </div>
+
+         <div class="info">
+            <strong>Justificativa:</strong> ${userInfo.justificativa}
+         </div>
+      </div>
+   </body>
+</html>
       `;
 
       const emailModel = {
          from: `${userInfo.nome_funcionario} <${userInfo.email}>`,
-         to: selectHighSchool(userInfo.to),
+         // to: selectHighSchool(userInfo.to),
+         to: "felipe524.fernandes@gmail.com",
          subject: "Justificativa de Ponto",
          text: formatedText,
-         html: `<pre>${formatedText}</pre>`,
+         html: formatedText,
       };
 
       const sender = transporter(userInfo);
